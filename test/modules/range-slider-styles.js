d08e447f66b7d528d9876a08fd3c9c15a5598b09
Tool Cool <toolcool.org@gmail.com>
2022-08-09 21:35:53 +0300

    tests

--- test/modules/range-slider-styles.js
+++ test/modules/range-slider-styles.js
@@ -208,3 +208,74 @@ QUnit.module('Pointer Background Hover', () => {
         assert.equal($slider.style.getPropertyValue('--toolcool-range-slider-pointer-bg-hover'), 'green');
     });
 });
+
+QUnit.module('Pointer Shadow', () => {
+
+    QUnit.test('pointer shadow by default should be undefined', (assert) => {
+        const $slider = document.querySelector('#slider-1');
+        assert.equal($slider.pointerShadow, undefined);
+    });
+
+    QUnit.test('provided pointer shadow should be none', (assert) => {
+        const $slider = document.querySelector('#slider-26');
+        assert.equal($slider.pointerShadow, 'none');
+    });
+
+    QUnit.test('pointer shadow changed to none', (assert) => {
+        const $slider = document.querySelector('#slider-26');
+        $slider.pointerShadow = 'none';
+        assert.equal($slider.pointerShadow, 'none');
+    });
+
+    QUnit.test('by default pointer shadow style is empty', (assert) => {
+        const $slider = document.querySelector('#slider-1');
+        assert.equal($slider.style.getPropertyValue('--toolcool-range-slider-pointer-shadow'), '');
+    });
+
+    QUnit.test('provided pointer shadow style should be none', (assert) => {
+        const $slider = document.querySelector('#slider-26');
+        assert.equal($slider.style.getPropertyValue('--toolcool-range-slider-pointer-shadow'), 'none');
+    });
+
+    QUnit.test('pointer shadow changed to "0 0 2px red" ---> style should change', (assert) => {
+        const $slider = document.querySelector('#slider-26');
+        $slider.pointerShadow = '0 0 2px red';
+        assert.equal($slider.style.getPropertyValue('--toolcool-range-slider-pointer-shadow'), '0 0 2px red');
+    });
+});
+
+
+QUnit.module('Pointer Shadow Hover', () => {
+
+    QUnit.test('pointer shadow hover by default should be undefined', (assert) => {
+        const $slider = document.querySelector('#slider-1');
+        assert.equal($slider.pointerShadowHover, undefined);
+    });
+
+    QUnit.test('provided pointer shadow hover should be none', (assert) => {
+        const $slider = document.querySelector('#slider-26');
+        assert.equal($slider.pointerShadowHover, 'none');
+    });
+
+    QUnit.test('pointer shadow hover changed to none', (assert) => {
+        const $slider = document.querySelector('#slider-26');
+        $slider.pointerShadowHover = 'none';
+        assert.equal($slider.pointerShadowHover, 'none');
+    });
+
+    QUnit.test('by default pointer shadow hover style is empty', (assert) => {
+        const $slider = document.querySelector('#slider-1');
+        assert.equal($slider.style.getPropertyValue('--toolcool-range-slider-pointer-shadow-hover'), '');
+    });
+
+    QUnit.test('provided pointer shadow hover style should be none', (assert) => {
+        const $slider = document.querySelector('#slider-26');
+        assert.equal($slider.style.getPropertyValue('--toolcool-range-slider-pointer-shadow-hover'), 'none');
+    });
+
+    QUnit.test('pointer shadow hover changed to "0 0 2px red" ---> style should change', (assert) => {
+        const $slider = document.querySelector('#slider-26');
+        $slider.pointerShadowHover = '0 0 2px red';
+        assert.equal($slider.style.getPropertyValue('--toolcool-range-slider-pointer-shadow-hover'), '0 0 2px red');
+    });
+});
