64f1bdc9d16da4ffd06b1783c51e7a2ea24a7cad
Tool Cool <toolcool.org@gmail.com>
2022-08-28 14:31:59 +0300

    unit tests for 'round' are updated

--- test/modules/range-slider-round.js
+++ test/modules/range-slider-round.js
@@ -1,14 +1,14 @@
 
 QUnit.module('Range Slider Round', () => {
 
-  QUnit.test('default round should be 5', (assert) => {
+  QUnit.test('default round should be 2', (assert) => {
     const $slider = document.querySelector('#slider-1');
-    assert.equal($slider.round, 5);
+    assert.equal($slider.round, 2);
   });
 
-  QUnit.test('provided round should be 2', (assert) => {
+  QUnit.test('provided round should be 4', (assert) => {
     const $slider = document.querySelector('#slider-82');
-    assert.equal($slider.round, 2);
+    assert.equal($slider.round, 4);
   });
 
   QUnit.test('provided round should be 0', (assert) => {
@@ -16,14 +16,14 @@ QUnit.module('Range Slider Round', () => {
     assert.equal($slider.round, 0);
   });
 
-  QUnit.test('if provided round is string it should be 5', (assert) => {
+  QUnit.test('if provided round is string it should be 2', (assert) => {
     const $slider = document.querySelector('#slider-84');
-    assert.equal($slider.round, 5);
+    assert.equal($slider.round, 2);
   });
 
-  QUnit.test('if provided value is negative it should be 5', (assert) => {
+  QUnit.test('if provided value is negative it should be 2', (assert) => {
     const $slider = document.querySelector('#slider-85');
-    assert.equal($slider.round, 5);
+    assert.equal($slider.round, 2);
   });
 
   QUnit.test('change round via API to 3', (assert) => {
@@ -38,10 +38,10 @@ QUnit.module('Range Slider Round', () => {
     assert.equal($slider.round, 4);
   });
 
-  QUnit.test('1.55678 when round is 2 ---> should be equal 1.56', (assert) => {
+  QUnit.test('1.55678 when round is 4 ---> should be equal 1.56', (assert) => {
     const $slider = document.querySelector('#slider-82'); // provided round is 2
-    $slider.value = 1.55678;
-    assert.equal($slider.value, 1.56);
+    $slider.value = 1.55678999;
+    assert.equal($slider.value, 1.5568);
   });
 
   QUnit.test('1.55678 when round is 2 ---> should be equal 2', (assert) => {
