db68ecb0d57ac921f106f5807410920385e34c48
Tool Cool <toolcool.org@gmail.com>
2022-09-08 17:10:25 +0300

    unit tests

---
+++ test/modules/12-disabled-enabled/pointer-1.test.js
@@ -0,0 +1,53 @@
+QUnit.module('Disabled / Enabled - Pointer 1', (hooks) => {
+  initFixtures(hooks, `
+     <toolcool-range-slider id="slider-1"></toolcool-range-slider>
+     <toolcool-range-slider id="slider-131" pointer1-disabled="true"></toolcool-range-slider>
+     <toolcool-range-slider id="slider-132" pointer1-disabled="false"></toolcool-range-slider>
+     <toolcool-range-slider id="slider-133" pointer1-disabled="test"></toolcool-range-slider>
+  `);
+
+  QUnit.test('if not provided ---> pointer1Disabled value should be false', (assert) => {
+    const $slider = document.querySelector('#slider-1');
+    assert.equal($slider.pointer1Disabled, false);
+  });
+
+  QUnit.test('pointer1Disabled = true', (assert) => {
+    const $slider = document.querySelector('#slider-131');
+    assert.equal($slider.pointer1Disabled, true);
+  });
+
+  QUnit.test('pointer1Disabled = false', (assert) => {
+    const $slider = document.querySelector('#slider-132');
+    assert.equal($slider.pointer1Disabled, false);
+  });
+
+  QUnit.test('pointer1Disabled = test ---> pointer1Disabled should be false', (assert) => {
+    const $slider = document.querySelector('#slider-133');
+    assert.equal($slider.pointer1Disabled, false);
+  });
+
+  QUnit.test('change pointer1Disabled via API to true', (assert) => {
+    const $slider = document.querySelector('#slider-1');
+    $slider.pointer1Disabled = true;
+    assert.equal($slider.pointer1Disabled, true);
+  });
+
+  QUnit.test('change pointer1Disabled via API to false', (assert) => {
+    const $slider = document.querySelector('#slider-131');
+    $slider.pointer1Disabled = false;
+    assert.equal($slider.pointer1Disabled, false);
+  });
+
+  QUnit.test('change pointer1Disabled via attribute to true', (assert) => {
+    const $slider = document.querySelector('#slider-1');
+    $slider.setAttribute('pointer1-disabled', 'true');
+    assert.equal($slider.pointer1Disabled, true);
+  });
+
+  QUnit.test('change pointer1Disabled via attribute to false', (assert) => {
+    const $slider = document.querySelector('#slider-131');
+    $slider.setAttribute('pointer1-disabled', 'false');
+    assert.equal($slider.pointer1Disabled, false);
+  });
+
+});
---
+++ test/modules/12-disabled-enabled/pointer-2.test.js
@@ -0,0 +1,53 @@
+QUnit.module('Disabled / Enabled - Pointer 2', (hooks) => {
+  initFixtures(hooks, `
+     <toolcool-range-slider id="slider-1"></toolcool-range-slider>
+     <toolcool-range-slider id="slider-134" pointer2-disabled="true" value1="10" value2="20"></toolcool-range-slider>
+     <toolcool-range-slider id="slider-135" pointer2-disabled="false" value1="10" value2="20"></toolcool-range-slider>
+     <toolcool-range-slider id="slider-136" pointer2-disabled="test" value1="10" value2="20"></toolcool-range-slider>
+  `);
+
+  QUnit.test('if not provided ---> pointer2Disabled value should be false', (assert) => {
+    const $slider = document.querySelector('#slider-1');
+    assert.equal($slider.pointer2Disabled, false);
+  });
+
+  QUnit.test('pointer2Disabled = true', (assert) => {
+    const $slider = document.querySelector('#slider-134');
+    assert.equal($slider.pointer2Disabled, true);
+  });
+
+  QUnit.test('pointer2Disabled = false', (assert) => {
+    const $slider = document.querySelector('#slider-135');
+    assert.equal($slider.pointer2Disabled, false);
+  });
+
+  QUnit.test('pointer2Disabled = test ---> pointer2Disabled should be false', (assert) => {
+    const $slider = document.querySelector('#slider-136');
+    assert.equal($slider.pointer2Disabled, false);
+  });
+
+  QUnit.test('change pointer2Disabled via API to true', (assert) => {
+    const $slider = document.querySelector('#slider-135');
+    $slider.pointer2Disabled = true;
+    assert.equal($slider.pointer2Disabled, true);
+  });
+
+  QUnit.test('change pointer2Disabled via API to false', (assert) => {
+    const $slider = document.querySelector('#slider-134');
+    $slider.pointer2Disabled = false;
+    assert.equal($slider.pointer2Disabled, false);
+  });
+
+  QUnit.test('change pointer2Disabled via attribute to true', (assert) => {
+    const $slider = document.querySelector('#slider-135');
+    $slider.setAttribute('pointer2-disabled', 'true');
+    assert.equal($slider.pointer2Disabled, true);
+  });
+
+  QUnit.test('change pointer2Disabled via attribute to false', (assert) => {
+    const $slider = document.querySelector('#slider-134');
+    $slider.setAttribute('pointer2-disabled', 'false');
+    assert.equal($slider.pointer2Disabled, false);
+  });
+
+});
