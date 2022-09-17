7a6c3c506885e2a39c194167fb08a16dff619ce8
Tool Cool <toolcool.org@gmail.com>
2022-09-17 21:40:03 +0300

    build system update - combine settings, build and watch to a single file

--- run/build.js
+++ run/build.js
@@ -1,9 +1,62 @@
-import settings from './settings.js';
 import esbuild from 'esbuild';
+import pcssPlugin from './esbuild-plugins/esbuild-pcss-plugin.js';
+import esbuildTemplateLiteralsPlugin from './esbuild-plugins/esbuild-template-literals-plugin.js';
+import esbuildCopyPlugin from './esbuild-plugins/esbuild-copy-plugin.js';
+import fs from 'fs';
+import path from 'path';
+
+const packageJson = fs.readFileSync(path.join(process.cwd(), './package.json'), 'utf-8');
+let version = '1.0.1';
+
+try {
+  const parsed = JSON.parse(packageJson);
+  version = parsed.version;
+} catch (ex) {}
+
+const settings = {
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  sourcemap: 'external',
+  minify: true,
+  target: ['es6'],
+  outfile: './examples/js/toolcool-range-slider.min.js',
+  loader: {
+    '.png': 'text',
+    '.svg': 'dataurl',
+  },
+  plugins: [pcssPlugin, esbuildTemplateLiteralsPlugin, esbuildCopyPlugin],
+  banner: {
+    js: `/* 
+Tool Cool Range Slider 
+Version: ${version}
+Documentation: https://github.com/toolcool-org/toolcool-range-slider 
+License: MIT License        
+Author: Tool Cool, toolcool.org@gmail.com>                          
+*/`,
+  },
+};
+
+const args = process.argv.slice(2);
+const watch = args.length > 1 && args[1].trim().toLowerCase() === 'watch';
+
+if(watch){
+  // ------------- watch ---------------
+  settings.watch = {
+    onRebuild(error, result) {
+
+      if (error){
+        // console.error(error);
+      }
+      else {
+        console.log('Succeeded.');
+      }
+    },
+  };
+}
 
 esbuild
-    .build(settings)
-    .then(result => {
-        console.log('Done.');
-    })
-    .catch(() => process.exit(1));
+  .build(settings)
+  .then(result => {
+    console.log(watch ? 'Watching...' : 'Done.');
+  })
+  .catch(() => process.exit(1));
