8aa9e2138be46aafc5d85e9ba3dc88424c4162f5
Tool Cool <toolcool.org@gmail.com>
2022-09-22 23:21:23 +0300

    docs

--- src/docs/generator/index.js
+++ src/docs/generator/index.js
@@ -1,55 +1,15 @@
 import path from 'path';
 import fs from 'fs';
 import fse from 'fs-extra';
-import MarkdownIt from 'markdown-it'; // https://github.com/markdown-it/markdown-it
-import classy from 'markdown-it-classy'; // https://github.com/andrey-p/markdown-it-classy
+// import classy from 'markdown-it-classy'; // https://github.com/andrey-p/markdown-it-classy
 import { compileClientSideCSS } from './css-provider.js';
 import { compileClientSideScripts } from './js-provider.js';
-import hljs from'highlight.js'; // https://highlightjs.org
+import { configureMarkdown, initMarkDown } from "./markdown-config.js"; // https://highlightjs.org
 
-const md = new MarkdownIt({
-  html: true, // Enable HTML tags in source
-  xhtmlOut: true, // Use '/' to close single tags (<br />).
-  breaks: false, // Convert '\n' in paragraphs into <br>
-  langPrefix: 'language-',  // CSS language prefix for fenced blocks. Can be
-                              // useful for external highlighters.
-  linkify: true, // Autoconvert URL-like text to links
-
-  // Enable some language-neutral replacement + quotes beautification
-  // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js
-  typographer: true,
-
-  // Highlighter function. Should return escaped HTML,
-  // or '' if the source string is not changed and should be escaped externally.
-  // If result starts with <pre... internal wrapper is skipped.
-  highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return '<pre class="hljs"><code>' +
-          hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
-          '</code></pre>';
-      } catch (__) {}
-    }
-
-    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
-  }
-});
-
+// markdown -------------------
+const md = initMarkDown();
 // md.use(classy);
-
-// https://github.com/markdown-it/markdown-it/blob/master/docs/examples/renderer_rules.md
-// https://markdown-it.github.io/
-const proxy = (tokens, idx, options, env, self) => self.renderToken(tokens, idx, options);
-const defaultBulletListOpenRenderer = md.renderer.rules.bullet_list_open || proxy;
-
-md.use((mdInstance) => {
-  md.renderer.rules.heading_open = function(tokens, idx, options, env, self) {
-    // Make your changes here ...
-    tokens[idx].attrJoin('class', 'text-3xl mb-4');
-    // ... then render it using the existing logic
-    return defaultBulletListOpenRenderer(tokens, idx, options, env, self)
-  };
-});
+configureMarkdown(md);
 
 const changeExtension = (filePath, newExtension) => {
   const basename = path.basename(filePath, path.extname(filePath));
---
+++ src/docs/generator/markdown-config.js
@@ -0,0 +1,62 @@
+import MarkdownIt from 'markdown-it'; // https://github.com/markdown-it/markdown-it
+import hljs from 'highlight.js';
+
+export const initMarkDown = () => {
+  return new MarkdownIt({
+    html: true, // Enable HTML tags in source
+    xhtmlOut: true, // Use '/' to close single tags (<br />).
+    breaks: false, // Convert '\n' in paragraphs into <br>
+    langPrefix: 'language-',  // CSS language prefix for fenced blocks. Can be
+    // useful for external highlighters.
+    linkify: true, // Autoconvert URL-like text to links
+
+    // Enable some language-neutral replacement + quotes beautification
+    // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js
+    typographer: true,
+
+    // Highlighter function. Should return escaped HTML,
+    // or '' if the source string is not changed and should be escaped externally.
+    // If result starts with <pre... internal wrapper is skipped.
+    highlight: (str, lang) => {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return `<pre class="hljs p-4 mb-4 rounded-md shadow-xl"><code>${ hljs.highlight(str, { language: lang, ignoreIllegals: true }).value }</code></pre>`;
+        }
+        catch (__) {}
+      }
+
+      return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    }
+  });
+};
+
+export const configureMarkdown = (md) => {
+
+  // https://github.com/markdown-it/markdown-it/blob/master/docs/examples/renderer_rules.md
+  // https://markdown-it.github.io/
+  const proxy = (tokens, idx, options, env, self) => self.renderToken(tokens, idx, options);
+
+  const heading_open = md.renderer.rules.heading_open || proxy;
+  const paragraph_open = md.renderer.rules.paragraph_open || proxy;
+  // const code_block = md.renderer.rules.code_block || proxy;
+
+  md.use((mdInstance) => {
+    // <h1>
+    md.renderer.rules.heading_open = function(tokens, idx, options, env, self) {
+      tokens[idx].attrJoin('class', 'text-3xl mb-8');
+      return heading_open(tokens, idx, options, env, self)
+    };
+
+    // <p>
+    md.renderer.rules.paragraph_open = function(tokens, idx, options, env, self) {
+      tokens[idx].attrJoin('class', 'mb-4');
+      return paragraph_open(tokens, idx, options, env, self)
+    };
+
+    // <code> ??
+    /*md.renderer.rules.code_block = function(tokens, idx, options, env, self) {
+      tokens[idx].attrJoin('class', 'mb-4');
+      return code_block(tokens, idx, options, env, self)
+    };*/
+  });
+};
