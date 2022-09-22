4cc648fceba91abbea67451d1c638213fed4c93b
Tool Cool <toolcool.org@gmail.com>
2022-09-22 22:42:35 +0300

    docs

--- src/docs/generator/index.js
+++ src/docs/generator/index.js
@@ -4,8 +4,35 @@ import fse from 'fs-extra';
 import MarkdownIt from 'markdown-it'; // https://github.com/markdown-it/markdown-it
 import { compileClientSideCSS } from './css-provider.js';
 import { compileClientSideScripts } from './js-provider.js';
+import hljs from'highlight.js'; // https://highlightjs.org
 
-const md = new MarkdownIt();
+const md = new MarkdownIt({
+  html: true, // Enable HTML tags in source
+  xhtmlOut: true, // Use '/' to close single tags (<br />).
+  breaks: false, // Convert '\n' in paragraphs into <br>
+  langPrefix: 'language-',  // CSS language prefix for fenced blocks. Can be
+                              // useful for external highlighters.
+  linkify: true, // Autoconvert URL-like text to links
+
+  // Enable some language-neutral replacement + quotes beautification
+  // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js
+  typographer: true,
+
+  // Highlighter function. Should return escaped HTML,
+  // or '' if the source string is not changed and should be escaped externally.
+  // If result starts with <pre... internal wrapper is skipped.
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+          hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+          '</code></pre>';
+      } catch (__) {}
+    }
+
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
+});
 
 const changeExtension = (filePath, newExtension) => {
   const basename = path.basename(filePath, path.extname(filePath));
