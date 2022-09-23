9fd34d4a998bb8322000d47622d3dd8fc73ece43
Tool Cool <toolcool.org@gmail.com>
2022-09-23 18:54:53 +0300

    docs

--- docs/js/index.1663947318845.js
+++
@@ -1,6 +0,0 @@
-/* 
-Tool Cool Range Slider Documentation
-Author: Tool Cool, toolcool.org@gmail.com>                          
-*/
-(()=>{var i=()=>{let n=document.getElementById("mobile-menu-btn");if(!n)return;n.addEventListener("click",e=>{e.stopPropagation(),document.body.classList.toggle("mobile-menu-opened")}),document.body.addEventListener("click",()=>{document.body.classList.remove("mobile-menu-opened")});let t=document.getElementById("side-menu");if(!t)return;t.addEventListener("click",e=>{e.stopPropagation()});let o=document.getElementById("mobile-menu-close-btn");!o||o.addEventListener("click",()=>{document.body.classList.remove("mobile-menu-opened")})},d=()=>{i()};d();})();
-//# sourceMappingURL=index.1663947318845.js.map
