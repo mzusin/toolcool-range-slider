b8195dc14784ee64ac7105255e26ab86263d736e
Miriam Zusin <miriamzusin@gmail.com>
2023-04-23 12:59:30 +0300

    update karma config

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    // autoWatch: true,
 
 
     // start these browsers
@@ -74,7 +74,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    // singleRun: false,
 
     // Concurrency level
     // how many browser instances should be started simultaneously
